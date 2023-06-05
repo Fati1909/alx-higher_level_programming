cdab9d08da0ad6d924b3aa7558820f6c7bec810b
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-06-05 15:39:12 -0700

    python

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - description
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: point to the next node
  */
@@ -15,6 +15,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
