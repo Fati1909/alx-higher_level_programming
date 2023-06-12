367f291f303528acbcc47ee66668c8e73d7421c3
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-06-12 16:33:07 -0700

    Data Structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """adds 2 tuples"""
+    """adds 2 tuples."""
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
@@ -15,4 +15,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])        
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
