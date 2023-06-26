94738584069ae59db0e61d31def9ec3524a96ccf
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-06-26 14:15:09 -0700

    python exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    """function that prints the first x elements of a list and only integers.
+    """print the first x elements of a list and only integers.
 
     Args:
         my_list (list): list to print.
-        x (int): num of elements of my_list to print.
+        x (int): num of elements to print.
 
     Returns:
         num of elements printed.
     """
 
-     ret = 0
+    ret = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
