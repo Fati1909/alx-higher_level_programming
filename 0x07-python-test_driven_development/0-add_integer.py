f12ae79de4abbf857c38fba5c9a0e7ac2c99bda7
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-07-05 02:41:19 -0700

    Python-Test Driven Development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,12 @@
 """an integer addition function."""
 
 def add_integer(a, b=98):
-    """Function that adds two integers"""
+    """Function that adds two integers.
+     Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
