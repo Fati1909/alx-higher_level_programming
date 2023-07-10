907019808d43408a98d60ff2f94594dfdcd04157
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-07-10 14:58:31 -0700

    Python-Inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance"""
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class."""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
