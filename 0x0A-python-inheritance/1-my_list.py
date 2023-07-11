287af734dfbd991d7fd267b504c619dfbd529dfd
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-07-10 17:25:13 -0700

    python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,7 @@
 """class MyList that inherits from list."""
 
 class MyList(list):
+    """implements sorted printing for the built-in list class"""
 
     def print_sorted(self):
         """prints the list in sorted ascending sort."""
