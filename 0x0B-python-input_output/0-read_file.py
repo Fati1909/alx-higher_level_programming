1536d3a6c55741a8f1ef9e6d7f106057673278a6
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-07-12 14:27:48 -0700

    Python- Input Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+def read_file(filename=""):
+    """print the content of a UTF8."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
