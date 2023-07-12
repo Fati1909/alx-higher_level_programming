ce648fdbc9295e20157ddebbe67ec4c1eaafe484
BENRKIA Fatima Zahrae <fatimazahrae.benrkia@gmail.com>
2023-07-12 14:51:45 -0700

    Python- Input Output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    """append a string at the end of a text file (UTF8)"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
