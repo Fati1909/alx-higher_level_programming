016c7fb1acff053950e8016a714379ebd664a9b2
BENRKIA <fatimazahrae.benrkia@gmail.com>
2023-09-29 16:15:16 -0700

    Python - Network #1

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print(f"Error code: {err.code}")
